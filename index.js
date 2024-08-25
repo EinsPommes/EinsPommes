fafadbd7585a7b9ba64fc4407f40276fe982f06b
ミカ <96911492+EinsPommes@users.noreply.github.com>
2024-08-25 17:06:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const readme = `\
 
 ⏰ Updated on ${new Date().toUTCString()}
 
-![Progress Bar CI](https://github.com/liununu/liununu/workflows/Progress%20Bar%20CI/badge.svg)\
+![Progress Bar CI](https://github.com/EinsPommes/EinsPommes/blob/main/.github/workflows/main.yml)\
 `
 
 console.log(readme)
